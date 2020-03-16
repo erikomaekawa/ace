d4ba3292015e8b6065f17ee9b5e61707adfc5858
erikomaekawa <49210001+erikomaekawa@users.noreply.github.com>
2020-03-15 23:27:04 -0700

    Add files via upload

---
+++ events/events.php
@@ -0,0 +1,305 @@
+  <!DOCTYPE html>
+
+  <html lang="en">
+
+  <head>
+      <title>Ace in the Hole Multisport Events</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="author" content="Eriko Maekawa">
+
+      <meta name="description" content=" Ace in the Hole Multisport Events is a traditional annual running and triathlon event for everybody from across the country. Please join our legendary event. Register now!">
+
+      <meta property=" og:title" content="Ace in the Hole Multisport Events">
+      <meta property="og:image" content="http://kakitty.webhostingforstudents.com/cas222/project/ace/images/ace4.jpg">
+      <meta property="og:image" content="http://kakitty.webhostingforstudents.com/cas222/project/ace/images/triathlon.jpg">
+      <meta property="og:description" content="A legendary annual running and triathlon event in Portland, Oregon.">
+
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159864977-1"></script>
+      <script>
+          window.dataLayer = window.dataLayer || [];
+
+          function gtag() {
+              dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+
+          gtag('config', 'UA-159864977-1');
+
+      </script>
+
+      <link href="../css/style.css" rel="stylesheet/less">
+
+      <link href="../css/navigation.css" rel="stylesheet" type="text/css">
+
+      <link href="../css/grid.css" rel="stylesheet" type="text/css">
+
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+
+      <link href="https://fonts.googleapis.com/css?family=Lato|Volkhov:400,700&display=swap" rel="stylesheet">
+
+      <script src="http://cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js"></script>
+
+
+  </head>
+
+  <body>
+
+      <?php include '../includes/header.inc.php'; ?>
+
+      <main>
+
+          <section>
+              <h1>Event Details</h1>
+          </section>
+
+          <section>
+              <img src="../images/triathlon.jpg" id="event-img" alt="Running athlete while high-fiving crowd">
+
+          </section>
+
+          <section>
+
+              <h2>About the Event</h2>
+              <p>The Annual Ace in the Hole Multisport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation.</p>
+
+
+              <p>There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win, but make sure you come to have fun!</p>
+
+          </section>
+
+          <section>
+
+              <h2 class="space">Event Schedule (3/28-29)</h2>
+
+              <h3 class="sat-sun">Saturday</h3>
+
+              <table>
+                  <tr>
+                      <th>Time</th>
+                      <th>Course</th>
+                  </tr>
+
+                  <tr>
+                      <td>7:00 AM</td>
+                      <td>Long Course Triathlon</td>
+                  </tr>
+
+                  <tr>
+                      <td>7:30 AM</td>
+                      <td>Olympic Triathlon</td>
+                  </tr>
+
+                  <tr>
+                      <td>7:15 AM</td>
+                      <td>10K</td>
+                  </tr>
+
+                  <tr>
+                      <td>7:15 AM</td>
+                      <td>Half Marathon</td>
+                  </tr>
+
+              </table>
+
+              <h3 class="sat-sun">Sunday</h3>
+
+              <table>
+                  <tr>
+                      <th>Time</th>
+                      <th>Course</th>
+                  </tr>
+
+                  <tr>
+                      <td>8:00 AM</td>
+                      <td>Sprint Triathlon</td>
+                  </tr>
+
+                  <tr>
+                      <td>8:20 AM</td>
+                      <td>Try-a-Tri</td>
+                  </tr>
+
+                  <tr>
+                      <td>12:00 PM</td>
+                      <td>Splash n' Dash</td>
+                  </tr>
+
+              </table>
+          </section>
+
+          <section>
+
+              <h2 class="space">Course Details</h2>
+              <p>WATER TEMPERATURE is expected to be between 62 & 66 degrees. The temperature will be taken on Friday and the morning of the race. Wetsuits are recommended.</p>
+
+              <h3>1. Long Course Triathlon</h3>
+
+              <ul>
+                  <p class="bold">Swim - 1.2mi</p>
+                  <p>Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p>
+
+
+                  <p class="bold">Bike - 58 Miles</p>
+                  <p>A scenic point-to-point course that travels over gently rolling hills prior to three hard climbs. The bike course will be marked with large directional signage and there will be course marshals at key intersections to help direct you.</p>
+
+                  <p class="bold">Run - 13.1mi</p>
+                  <p>A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (two hills total with a minimal elevation gain).</p>
+              </ul>
+
+              <h3>2. Olympic Triathlon</h3>
+
+              <ul>
+                  <p class="bold">Swim - 1,500 meters</p>
+                  <p>Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p>
+
+                  <p class="bold">Bike - 28mi</p>
+                  <p>A scenic point-to-point course that travels over gently rolling hills. The bike course will be marked with large directional signage and course marshals will be present at key intersections.</p>
+
+                  <p class="bold">Run - 10K</p>
+                  <p>A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).</p>
+              </ul>
+
+              <h3>3. Sprint Triathlon</h3>
+
+              <p>Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike Course as the Olympic distance and a 5km run.</p>
+
+              <h3>4. Try-A-Tri</h3>
+
+              <p>This novice race is designed for the first time triathlete, those new to the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance, (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2 loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).</p>
+
+              <h3>5. Half Marathon Course</h3>
+              <p>Half-Marathon (13.1-miles): Half-Marathon event starts and finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can enjoy the finish line festivities, including the Sports & Fitness Expo and live entertainment. Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin once the results have been certified.</p>
+
+              <h3>6. 10k Course</h3>
+              <p>The 10K event starts and finishes in the Athletes Village. The paths are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to the roaring cheers of the crowd.</p>
+
+              <h3>7. Splash n' Dash</h3>
+              <p>Kids event for participants ages 4 to 15. This is a 100 yard swim and a 50 yard dash. It is free if an adult registers for an event otherwise it is $25.</p>
+
+          </section>
+
+          <section>
+
+              <h2 class="space">What to Bring</h2>
+
+              <p>Watch the weather closely. The show goes on no matter what the weather is doing.</p>
+
+              <p>You must wear your chip timing piece during the entire event. You will be given a band that will hold your chip timing piece around your ankle throughout the entire event. Be sure that it is snapped tightly. Be sure to have your Chip Timing piece on before you start the race and be sure to step over the timing mats after each segment of the race. If you are wearing a wet-suit, make sure the timing piece goes under your wetsuit otherwise, you will have a very difficult time getting off your wet suit.</p>
+
+              <p><span class="bold">Swim:</span> Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many people opt for no wetsuit for a triathlon so no worries. We will provide you with a swim cap but you will want to bring your own goggles.</p>
+
+              <p><span class="bold">Bike:</span> A biking helmet is mandatory. You will also receive 2 stickers in your package with your race number on them. The small sticker should go on the front of your helmet. The bigger sticker will wrap around your bike frame. Road or mountain bikes are acceptable. For safety reasons, be sure to have end-caps on the end of your handle-bars.</p>
+
+              <p><span class="bold">Run:</span> You must finish the race with your bib number on the front of you. Some people choose to pin it on at the beginning of the race and have it on for the whole event so they don't have to worry about it. Others pin it on a singlet that they put on once they finish the swim before they head out for the bike. Others use an elastic racing strap that they pin their bib number to and then quickly strap it on before they leave for the run. Use whatever option feels best for you.</p>
+
+              <p> Remember to bring a change of clothing so you can enjoy the post-event festivities.</p>
+
+          </section>
+
+          <section>
+
+              <h2 class="space">Participation Fee</h2>
+
+              <table>
+                  <tr>
+                      <th>Course</th>
+                      <th>Cost</th>
+                  </tr>
+
+                  <tr>
+                      <td>Long Course Triathlon</td>
+                      <td>$240</td>
+                  </tr>
+
+                  <tr>
+                      <td>Olympic Triathlon</td>
+                      <td>$110</td>
+                  </tr>
+
+                  <tr>
+                      <td>10K</td>
+                      <td>$50</td>
+                  </tr>
+
+                  <tr>
+                      <td>Half Marathon</td>
+                      <td>$75</td>
+
+                  <tr>
+                      <td>Sprint Triathlon</td>
+                      <td>$90</td>
+                  </tr>
+
+                  <tr>
+                      <td>Try-a-Tri</td>
+                      <td>$110</td>
+                  </tr>
+
+                  <tr>
+                      <td>Splash n' Dash</td>
+                      <td>$25 / Free</td>
+                  </tr>
+
+              </table>
+
+              <p id="splash_detail">*Splash n' Dash is free for children (ages 4-15) if an adult registers for an event.</p>
+
+              <h3>Cost Includes</h3>
+              <ul>
+                  <li>Food & Beer</li>
+                  <li>Access to the weekend's live entertainment & Fitness Expo</li>
+                  <li>Commemorative Finisher medal</li>
+                  <li>Accurate Chip Timing for competitive races</li>
+                  <li>Ace in the Hole Multisport Weekend Tech Shirt</li>
+                  <li>Post-event party & entertainment</li>
+              </ul>
+              NOTE: Tech shirts guaranteed to pre-registered participants only.
+
+          </section>
+
+          <section>
+              <h2 class="space">Packet Pick Up</h2>
+              <p>All packet pick up will occur at Why Worry Racing, 123 NW Everett, Portland OR. Packet pick up hours: Thursday 8-5, Friday 9-noon. No day of event packet pick up.</p>
+
+              <h2 class="space">Have a Question?</h2>
+              <p>Please check our <span class="bold">
+
+                      <a href="<?php echo BASE_URL; ?>events/faq.php">FAQs (Frequently Asked Questions)</a></span></p><br>
+          </section>
+
+          <section>
+              <p class="logo2"><a href="<?php echo BASE_URL; ?>register">
+                      <img onclick="homepageLink();" src="<?php echo BASE_URL; ?>/images/rsz_register2.png" class="logo2" alt="The button of registration"></a></p>
+          </section>
+
+          <section id="weather">
+              <div id="wrapper">
+                  <a class="weatherwidget-io" href="https://forecast7.com/en/40d71n74d01/new-york/?unit=us" data-label_1="Portland" data-label_2="WEATHER" data-theme="original">Portland WEATHER</a>
+
+                  <script>
+                      ! function(d, s, id) {
+                          var js, fjs = d.getElementsByTagName(s)[0];
+                          if (!d.getElementById(id)) {
+                              js = d.createElement(s);
+                              js.id = id;
+                              js.src = 'https://weatherwidget.io/js/widget.min.js';
+                              fjs.parentNode.insertBefore(js, fjs);
+                          }
+                      }(document, 'script', 'weatherwidget-io-js');
+
+                  </script>
+              </div>
+          </section>
+      </main>
+
+      <?php include '../includes/footer.inc.php'; ?>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="../scripts/script.js"></script>
+
+  </body>
+
+  </html>
---
+++ events/faq.php
@@ -0,0 +1,93 @@
+  <!DOCTYPE html>
+
+  <html lang="en">
+
+  <head>
+      <title>Ace in the Hole Multisport Events</title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="author" content="Eriko Maekawa">
+
+      <meta name="description" content=" Ace in the Hole Multisport Events is a traditional annual running and triathlon event for everybody from across the country. Please join our legendary event. Register now!">
+
+      <meta property=" og:title" content="Ace in the Hole Multisport Events">
+      <meta property="og:image" content="http://kakitty.webhostingforstudents.com/cas222/project/ace/images/ace4.jpg">
+      <meta property="og:image" content="http://kakitty.webhostingforstudents.com/cas222/project/ace/images/triathlon.jpg">
+      <meta property="og:description" content="A legendary annual running and triathlon event in Portland, Oregon.">
+
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159864977-1"></script>
+      <script>
+          window.dataLayer = window.dataLayer || [];
+
+          function gtag() {
+              dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+
+          gtag('config', 'UA-159864977-1');
+
+      </script>
+
+      <link href="../css/style.css" rel="stylesheet/less">
+
+      <link href="../css/navigation.css" rel="stylesheet" type="text/css">
+
+      <link href="../css/grid.css" rel="stylesheet" type="text/css">
+
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+
+      <link href="https://fonts.googleapis.com/css?family=Lato|Volkhov:400,700&display=swap" rel="stylesheet">
+
+      <script src="http://cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js"></script>
+
+
+  </head>
+
+  <body>
+
+      <?php include '../includes/header.inc.php'; ?>
+
+      <main>
+
+          <section>
+              <h1 id="faq">Frequently Asked Questions</h1>
+
+              <h3>What are the Rules? </h3>
+              <p class="faq">We currently adhere to the USAT Rules for Triathlon & Duathlon. Important rules include no drafting, you must wear a helmet and music is ABSOLUTELY NOT allowed during the bike for obvious safety reasons.</p>
+
+              <h3>Can I use a personal music device while cycling?</h3>
+              <p class="faq">Absolutely no music devices are allowed during the bike segment for obvious safety risks and will result in immediate disqualification.</p>
+
+              <h3>Can I use a personal music device while running?</h3>
+              <p class="faq">During any running segments, we prefer for athletes to NOT use music devices to maximize safety, assure a fair competitive environment and assure athletes can hear all course marshal instructions. With that said, we also understand that many athletes rely on music to help endure the challenge of running. To accommodate the needs of our athletes while still assuring maximum safety and a fair, competitive environment, here is the arrangement that we can accommodate.</p>
+
+              <ul id="list">
+                  <li>If you chose to use a personal music device, you must always keep one ear open for instructions so only one ear-bud can be used at any time. In addition, if you opt to use music, you will not be eligible for awards, points and rankings. So as an athlete, you have to make a choice - music or awards/points/rankings.</li><br>
+
+                  <li>If you are using a personal music devise with two earbuds, you will be immediately disqualified. Trust us, we don't want to have to enforce a disqualification but in order to assure the safety of all athletes, this is a very important rule.</li><br>
+
+                  <li>We also request that if you chose to use a personal music device that you keep the volume to a minimum to assure all instructions can be heard.</li>
+              </ul>
+
+              <h3>Do I need to wear a wetsuit?</h3>
+              <p class="faq">No, you do not need to wear a wetsuit. Many will not wear a wetsuit while others will choose to wear a wetsuit because of the buoyancy and warmth factor.</p>
+
+              <h3>Do I have to use a road or racing bike?</h3>
+              <p class="faq">No. We welcome any type of bike as long as it is functioning properly with brakes and endcaps at the end of your handlebars.</p>
+
+              <p id="go_contact">If you have any other questions, please <a href="<?php echo BASE_URL; ?>contact"><span class="bold">Contact Us</span></a>.</p><br>
+
+          </section>
+
+      </main>
+
+      <?php include '../includes/footer.inc.php'; ?>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="../scripts/script.js"></script>
+
+  </body>
+
+  </html>
