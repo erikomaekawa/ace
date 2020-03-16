09947f989b988e0b1a133f954cf0e21a7234b07a
erikomaekawa <49210001+erikomaekawa@users.noreply.github.com>
2020-03-15 23:27:39 -0700

    Add files via upload

---
+++ home/home.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>Ace in the Hole Multisport Events</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Eriko Maekawa">
+
+    <meta name="description" content=" Ace in the Hole Multisport Events is a traditional annual running and triathlon event for everybody from across the country. Please join our legendary event. Register now!">
+
+    <meta property="og:title" content="Ace in the Hole Multisport Events">
+    <meta property="og:image" content="http://kakitty.webhostingforstudents.com/cas222/project/ace/images/ace4.jpg">
+    <meta property="og:image" content="http://kakitty.webhostingforstudents.com/cas222/project/ace/images/triathlon.jpg">
+    <meta property="og:description" content="A legendary annual running and triathlon event in Portland, Oregon.">
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159864977-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-159864977-1');
+
+    </script>
+
+    <link href="css/style.css" rel="stylesheet/less">
+
+    <link href="css/navigation.css" rel="stylesheet" type="text/css">
+
+    <link href="css/grid.css" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato|Volkhov:400,700&display=swap" rel="stylesheet">
+
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js"></script>
+
+
+</head>
+
+<body>
+
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0"></script>
+
+    <?php include 'includes/header.inc.php'; ?>
+
+    <main>
+
+        <section id="ace_title">
+            <h1>Ace in the Hole Multisport Events</h1>
+        </section>
+
+        <section>
+
+            <div class="slide-show">
+
+                <img class="mySlides" src="images/ace4.jpg" alt=" Athletes running on the beach">
+                <img class="mySlides" src="images/ace5.jpg" alt="Athletes bicycling with blue sky">
+                <img class="mySlides" src="images/ace18.jpg" alt="Athletes running ">
+                <img class="mySlides" src="images/ace12.jpg" alt="Athletes bicycling ">
+            </div>
+
+
+        </section>
+
+        <section>
+
+            <h2>About Us</h2>
+            <p>Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, gender identifications and cultural backgrounds. We offer Events for Every Body.</p>
+            <p>We hold a popular annual running and triathlon event during the last weekend of March. To learn more about the event details, check out the <a href="<?php echo BASE_URL; ?>events">Event Details</a>.</p><br>
+
+            <h2>Follow Us on Social Media</h2>
+
+            <section class="container">
+                <section class="row">
+
+                    <div class="section group">
+                        <div class="col span_1_of_2 twitter">
+                            <h3 id="twitter">Twitter</h3>
+                            <div class="border">
+                                <a class="twitter-timeline" href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw" data-tweet-limit="4">Tweets by pcccas222</a>
+                                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                            </div>
+                        </div>
+
+                        <div class="col span_1_of_2">
+                            <h3 id="facebook">Facebook</h3>
+                            <div class="fb-page" data-href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" data-tabs="timeline" data-width="432" data-height="614" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true">
+                                <blockquote cite="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/">Cas222Aceinthehole</a></blockquote>
+                            </div>
+                        </div>
+                    </div>
+
+                </section>
+            </section>
+
+        </section>
+    </main>
+
+    <?php include 'includes/footer.inc.php'; ?>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="scripts/script.js"></script>
+
+</body>
+
+</html>
